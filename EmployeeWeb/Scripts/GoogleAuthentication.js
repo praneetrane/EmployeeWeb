1f916bd853f3a70b8379cf6987d8281b4fe8adbc
PRANEET <praneet.rane@gmail.com>
2019-06-23 20:07:24 +0530

    Google Authentication complete

--- EmployeeWeb/Scripts/GoogleAuthentication.js
+++ EmployeeWeb/Scripts/GoogleAuthentication.js
@@ -2,43 +2,43 @@
 
 function getAccessToken(){
     if (location.hash){
-        if (location.hash.split('acess_token=')){
-            var acessToken=location.hash.split('acess_token=')[1].split('&')[0];
-            if (acessToken){
-                isUserRegistered(acessToken);
+        if (location.hash.split('access_token=')){
+            var accessToken = location.hash.split('access_token=')[1].split('&')[0];
+            if (accessToken) {
+                isUserRegistered(accessToken);
             }
         }
     }
 }
 
-function isUserRegistered(acessToken) {
+function isUserRegistered(accessToken) {
     $.ajax({
         url: '/api/Account/UserInfo',
         method: 'GET',
         headers: {
             'content-type': 'application/JSON',
-            'Authorization' : 'Bearer '+ acessToken
+            'Authorization': 'Bearer ' + accessToken
         },
         success: function (response) {
             if (response.HasRegistered) {
-                localStorage.setItem('accessToken', acessToken);
+                localStorage.setItem('accessToken', accessToken);
                 localStorage.setItem('userName', response.Email);
                 window.location.href = "Data.html";
             }
             else {
-                signupExternalUser(acessToken);
+                signupExternalUser(accessToken);
             }
         }
    })
 }
 
-function signupExternalUser(acessToken) {
+function signupExternalUser(accessToken) {
     $.ajax({
         url: '/api/Account/RegisterExternal',
         method: 'POST',
         headers: {
             'content-type': 'application/JSON',
-            'Authorization': 'Bearer ' + acessToken
+            'Authorization': 'Bearer ' + accessToken
         },
         success: function (response) {
             window.location.href = "/api/Account/ExternalLogin?provider=Google&response_type=token&client_id=self&redirect_uri=http%3A%2F%2Flocalhost%3A51560%2Flogin.html&state=j-F8WhydB_ohZ8rvwwU95SjbG4nZawMod6OV1EpXnuU1";
